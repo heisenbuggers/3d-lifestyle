cf525bf612faba83eb0c797a520e640ce7a70f78
Boopathi Rajaa <me@boopathi.in>
2014-08-08 08:45:01 +0530

    Some changes

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 var express = require('express'),
+	bodyParser = require('body-parser'),
+	favicon = require('serve-favicon'),
+	methodOverride = require('method-override'),
 	http = require("http"),
     path = require("path"),
 	app = express(),
 	routes = require("./routes");
 
-app.use(bodyParser());
-
 var port = process.env.PORT || 8082;
 
 app.set('views', path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.favicon());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser(config[env].SECRET));
+//app.use(favicon());
+app.use(bodyParser());
+app.use(methodOverride());
 
 app.use(express.static(path.join(__dirname, 'public', 'build')));
 
 app.get('/video', routes.video);
 
 var server = http.createServer(app);
-app.use(app.router);
 
 server.listen(port, "0.0.0.0", function() {
 	console.log("3D lifestyle server listening on port ", port);
-})
+});
