0ec0132a05273f8d31a210caad4fd92d56c51872
Boopathi Rajaa <me@boopathi.in>
2014-08-08 13:09:26 +0530

    asdf

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
+var fs = require('fs');
+
 var video = function(req, res) {
 	res.render('video', {
-		//src: "http://cloud.stylista.com/videos/2013/Sep/1379353232_Stylista%20-%203989.webm"
-		src: "http://cloud.stylista.com/videos/2013/Oct/1380569207_Stylista - 4141.webm"
+		src: fs.readdirSync('')
 	});
 };
 
